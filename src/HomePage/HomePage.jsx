50dbca4059d22d634aaa8f327020f3c7a7271052
wahidgoni17 <imwahidgoni17@gmail.com>
2023-08-10 18:16:20 +0600

    basic setup

---
+++ src/HomePage/HomePage.jsx
@@ -0,0 +1,15 @@
+import Footer from "@/Components/Footer";
+import Navbar from "@/Components/Navbar";
+import React from "react";
+
+const HomePage = () => {
+  return (
+    <>
+      <Navbar />
+      <h1>this is HomePage</h1>
+      <Footer />
+    </>
+  );
+};
+
+export default HomePage;
