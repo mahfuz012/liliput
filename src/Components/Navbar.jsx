e198b5f36d4cc372cb2fb4723b787ee604b399c6
wahidgoni17 <imwahidgoni17@gmail.com>
2023-08-10 19:27:05 +0600

    add  daisyui

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -1,11 +1,11 @@
-import React from 'react';
+import React from "react";
 
 const Navbar = () => {
-    return (
-        <>
-            
-        </>
-    );
+  return (
+    <>
+      
+    </>
+  );
 };
 
 export default Navbar;
