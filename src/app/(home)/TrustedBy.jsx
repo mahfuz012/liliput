22fd8056732a34f11ebf3df850e3e87783ed2305
mdshahed <mdshahedrahman14@gmail.com>
2023-08-18 17:14:49 +0600

    trusted com added

---
+++ src/app/(home)/TrustedBy.jsx
@@ -0,0 +1,69 @@
+import React from 'react';
+
+const TrustedBy = () => {
+    return (
+        <div className="mx-auto px-2">
+        <h1 className="text-6xl font-serif font-medium">
+          Trusted by leading brands and startups
+        </h1>
+        <div className=" gap-6 md:flex justify-between items-center">
+          <div className="bg-[#13544E] card card-compact mt-4 sm:h-[700px] lg:h-[500px] w-full rounded-lg p-5 text-white font-medium">
+            <h1 className="my-3 ml-5 text-2xl">Nasdaq</h1>
+            <h1 className="my-3 font-serif  text-2xl">
+              “Flow enables us to differentiate ourselves from our competitors and
+              produce content at a higher caliber.”
+            </h1>
+            <h1 className=" text-[#D5E0D5] text-xl">
+              Josh Machiz, Chief Digital Officer
+            </h1>
+            <p className=" text-[#D5E0D5] text-xl">Results</p>
+            <hr className="my-1" />
+            <h1 className="my-3 font-serif  text-2xl">Emmy Winning</h1>
+            <p className=" text-[#D5E0D5] text-xl">Facebook Watch program</p>
+            <h1 className="my-3 font-serif  text-2xl">Millions</h1>
+            <p className=" text-[#D5E0D5] text-xl">
+              of impressions generated per client per IPO
+            </p>
+          </div>
+          <div className="bg-[#BC511B] card card-compact mt-4 sm:h-[700px] lg:h-[500px] w-full rounded-lg p-5 text-white font-medium">
+            <h1 className="my-3 ml-5 text-2xl">Microsoft</h1>
+            <h1 className="my-3 font-serif  text-2xl">
+              “One of the advantages of utilizing freelancers is finding talent
+              with different skills quickly as our needs change.”
+            </h1>
+            <h1 className=" text-[#D5E0D5] text-xl">
+              Carol Taylor, Director of Content Experience 
+            </h1>
+            <p className=" text-[#D5E0D5] text-xl">Results</p>
+            <hr className="my-1" />
+            <h1 className="my-3 font-serif  text-2xl">50% Faster</h1>
+            <p className=" text-[#D5E0D5] text-xl">launch of projects</p>
+            <h1 className="my-3 font-serif  text-2xl">10,000</h1>
+            <p className=" text-[#D5E0D5] text-xl">
+            projects completed
+            </p>
+          </div>
+          <div className="bg-[#001E00] card card-compact mt-4 sm:h-[700px] lg:h-[500px] w-full rounded-lg p-5 text-white font-medium">
+            <h1 className="my-3 ml-5 text-2xl">Google</h1>
+            <h1 className="my-3 font-serif  text-2xl">
+              “Clients love Flow.
+  On the site, freelance professionals can browse jobs posted by prospective clients from all over the world. ”
+            </h1>
+            <h1 className=" text-[#D5E0D5] text-xl">
+              Mr James, Director of Content Experience 
+            </h1>
+            <p className=" text-[#D5E0D5] text-xl">Results</p>
+            <hr className="my-1" />
+            <h1 className="my-3 font-serif  text-2xl">Very Good</h1>
+            <p className=" text-[#D5E0D5] text-xl">launch of projects</p>
+            <h1 className="my-3 font-serif  text-2xl">20,000</h1>
+            <p className=" text-[#D5E0D5] text-xl">
+            projects completed
+            </p>
+          </div>
+        </div>
+      </div>
+    );
+};
+
+export default TrustedBy;
