26d17da9a966763f6fd5baba09fb96a2456aa5b5
wahidgoni17 <imwahidgoni17@gmail.com>
2023-08-21 19:14:30 +0600

    merge with main

--- src/app/register/page.js
+++ src/app/register/page.js
@@ -7,9 +7,8 @@ import { useContext } from "react";
 import { useForm } from "react-hook-form";
 import swal from "sweetalert";
 
-const ragistraionfiles = () => {
+const RegistrationPage = () => {
   const { createRegister, userProfile } = useContext(AuthContextPro);
-
   const {
     reset,
     register,
@@ -74,113 +73,108 @@ const ragistraionfiles = () => {
   const password = watch("password");
 
   return (
-    <div className="mb-20 border">
-      <div className="sm:flex border-2 sm:justify-center ">
-        <div
-          style={{ alignItems: "center" }}
-          className="sm:w-6/12  px-10 sm:px-0  sm:flex "
+    <div className="my-10 container mx-auto border">
+      <div className="flex justify-center py-10 border-2">
+        <form
+          onSubmit={handleSubmit(submitFunction)}
+          className="p-5 text-center "
         >
-          <form
-            onSubmit={handleSubmit(submitFunction)}
-            className="my-5 py-5 sm:text-center "
-          >
-            <h1 className="text-gray-600 pb-5 text-3xl font-bold mb-2">
-              Registration
-            </h1>
-            <div style={{ alignItems: "center" }} className="flex  flex-col">
-              <input
-                {...register("text", { required: true })}
-                className="border border-blue-300 w-full sm:w-80 my-2 p-2 "
-                type="text"
-                placeholder="Type your Name"
-                name="text"
-              />
-              {errors.text && (
-                <span className="text-sm text-red-700 ">
-                  Please fill this field
-                </span>
-              )}
+          <h1 className="text-gray-600 pb-5 text-3xl font-bold mb-2">
+            Registration
+          </h1>
+          <div className="flex flex-col items-center">
+            <input
+              {...register("text", { required: true })}
+              className="border border-blue-300 w-full sm:w-80 my-2 p-2 "
+              type="text"
+              placeholder="Type your Name"
+              name="text"
+            />
+            {errors.text && (
+              <span className="text-sm text-red-700 ">
+                Please fill this field
+              </span>
+            )}
 
-              <input
-                {...register("email", { required: true })}
-                className="border border-blue-300  w-full sm:w-80 my-2 p-2 "
-                type="email"
-                placeholder="Type your Email"
-                name="email"
-              />
-              {errors.email && (
-                <span className="text-sm text-red-700 ">
-                  Please fill this field
-                </span>
-              )}
+            <input
+              {...register("email", { required: true })}
+              className="border border-blue-300  w-full sm:w-80 my-2 p-2 "
+              type="email"
+              placeholder="Type your Email"
+              name="email"
+            />
+            {errors.email && (
+              <span className="text-sm text-red-700 ">
+                Please fill this field
+              </span>
+            )}
 
-              <input
-                {...register("url")}
-                className="border border-blue-300 w-full sm:w-80 my-2 p-2 "
-                type="url"
-                placeholder="Type your Profile URL"
-                name="url"
-              />
-              {errors.url && (
-                <span className="text-sm text-red-700 ">
-                  Please fill this field
-                </span>
-              )}
+            <input
+              {...register("url")}
+              className="border border-blue-300 w-full sm:w-80 my-2 p-2 "
+              type="url"
+              placeholder="Type your Profile URL"
+              name="url"
+            />
+            {errors.url && (
+              <span className="text-sm text-red-700 ">
+                Please fill this field
+              </span>
+            )}
 
-              <input
-                {...register("password", {
-                  required: true,
-                  validate: validatePassword,
-                })}
-                className=" border-blue-300 w-full sm:w-80 border my-2 p-2"
-                type="password"
-                placeholder="Type your password"
-                name="password"
-              />
-              {errors.password && (
-                <span className="text-sm text-red-700 ">
-                  {errors.password.message}
-                </span>
-              )}
+            <input
+              {...register("password", {
+                required: true,
+                validate: validatePassword,
+              })}
+              className=" border-blue-300 w-full sm:w-80 border my-2 p-2"
+              type="password"
+              placeholder="Type your password"
+              name="password"
+            />
+            {errors.password && (
+              <span className="text-sm text-red-700 ">
+                {errors.password.message}
+              </span>
+            )}
 
-              <input
-                {...register("confirm", {
-                  required: true,
-                  validate: (value) =>
-                    value === password || "Passwords don't match",
-                })}
-                className=" border-blue-300 w-full sm:w-80 border my-2 p-2"
-                type="password"
-                placeholder="Confirm your password"
-                name="confirm"
-              />
-              {errors.confirm && (
-                <span className="text-sm text-red-700 ">
-                  {errors.confirm.message}
-                </span>
-              )}
+            <input
+              {...register("confirm", {
+                required: true,
+                validate: (value) =>
+                  value === password || "Passwords don't match",
+              })}
+              className=" border-blue-300 w-full sm:w-80 border my-2 p-2"
+              type="password"
+              placeholder="Confirm your password"
+              name="confirm"
+            />
+            {errors.confirm && (
+              <span className="text-sm text-red-700 ">
+                {errors.confirm.message}
+              </span>
+            )}
 
-              <input
-                value="Register"
-                className="w-52  my-2 p-2  bg-blue-600 text-white"
-                type="submit"
-              />
-            </div>
+            <input
+              value="Register"
+              className="w-52  my-2 p-2  bg-blue-600 text-white"
+              type="submit"
+            />
+          </div>
 
-            <p className="mt-2 mb-3 px-12 sm:px-5">
-              if you have registered account before, click
-              <Link
-                href={"/login"}
-                className="font-bold text-xl mx-1 text-blue-700"
-              >
-                Login
-              </Link>
-            </p>
-          </form>
-        </div>
+          <p className="mt-2 mb-3 px-12 sm:px-5">
+            if you have registered account before, click
+            <Link
+              href={"/login"}
+              className="font-bold text-xl mx-1 text-blue-700"
+            >
+              Login
+            </Link>
+          </p>
+        </form>
       </div>
     </div>
   );
 };
 
-export default ragistraionfiles;
+export default RegistrationPage;
