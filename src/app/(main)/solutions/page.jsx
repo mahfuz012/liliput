ae2af2f69b0acb2b96bcc0a0cb5df4e342f2b037
wahidgoni17 <imwahidgoni17@gmail.com>
2023-08-29 12:41:55 +0600

    merge with login branch

--- src/app/(main)/solutions/page.jsx
+++ src/app/(main)/solutions/page.jsx
@@ -8,24 +8,26 @@ export const metadata = {
 
 const SolutionsPage = () => {
   return (
-    <div className="mb-6 container mx-auto">
-      <div className="bg-gradient-to-br text-center text-white py-32 p-6 from-[#363E51] to-[#363E51]">
-        <h1 className="text-[40px] mb-5 font-bold">Work Your Way With Agreements</h1>
-        <h1 className="font-bold">
+    <div className="">
+      <div className="bg-gradient-to-t from-cyan-200 to-cyan-50 text-center py-16">
+        <h1 className="text-[26px] lg:text-[40px] mb-3 font-bold">
+          Work Your Way With Agreements
+        </h1>
+        <h1 className="px-3">
           We offer flexibility with our Agreements so that you can define
           exactly how you want to work online.
         </h1>
       </div>
-      <div className="text-center mb-7">
+      <div className="text-center my-10">
         <h1 className=" text-3xl font-bold mt-12  m-4">Payment Terms</h1>
-        <h1 className=" border-b-blue-600  w-12 border-2  my-4  mx-auto"></h1>
+        <h1 className=" border-b-blue-600  w-12 border-2 my-4 mx-auto"></h1>
         <h1>Choose how and when payments will be made.</h1>
       </div>
-      <div className="grid mb-14 px-4 grid-cols-1 md:grid-cols-2 gap-9 lg:gap-14">
-        <div className="card border-t-4 relative  border-[#2777C6] h-80 lg:h-72  pt-8 bg-[#FFFFFF] rounded-none card-compact  shadow-slate-400 shadow-xl">
+      <div className="grid mb-14 px-5 lg:px-10 grid-cols-1 md:grid-cols-2 gap-9 lg:gap-14">
+        <div className="card border-t-2 relative  border-[#2777C6] h-80 lg:h-72  pt-8 bg-[#FFFFFF] rounded-none card-compact shadow-xl">
           <div className="px-4">
-            <div className="mt-2">
-              <MdOutlinePriceChange className="w-16 text-[#2777C6] mx-auto h-16"></MdOutlinePriceChange>
+            <div className="my-2">
+              <MdOutlinePriceChange className="w-16 text-[#2777C6] mx-auto h-16" />
             </div>
             <div className="">
               <h1 className="font-bold tex-2xl my-1"> Fixed Price</h1>
@@ -35,15 +37,15 @@ const SolutionsPage = () => {
               </p>
             </div>
           </div>
-          <div className="bg-[#F7F7F7] absolute w-full bottom-0  py-4">
+          <div className="bg-gray-100 absolute w-full bottom-0  py-4">
             <p className="px-4">
               Ideal for: Jobs with high budget consideration
             </p>
           </div>
         </div>
-        <div className="card border-t-4  h-80 lg:h-72  border-[#2777C6] relative pt-8 bg-[#FFFFFF]  rounded-none card-compact  shadow-slate-400 shadow-xl">
+        <div className="card border-t-2 h-80 lg:h-72  border-[#2777C6] relative pt-8 bg-[#FFFFFF]  rounded-none card-compact shadow-xl">
           <div className="px-4">
-            <div className="mt-2">
+            <div className="my-2">
               <BsHourglassSplit className="w-16 text-[#2777C6] mx-auto h-16"></BsHourglassSplit>
             </div>
             <div className="">
@@ -55,16 +57,16 @@ const SolutionsPage = () => {
               </p>
             </div>
           </div>
-          <div className="bg-[#F7F7F7] absolute w-full bottom-0   py-4">
+          <div className="bg-gray-100 absolute w-full bottom-0   py-4">
             <p className="px-4 ">
               Ideal for: Jobs which need flexibility with evolving job
               specifications
             </p>
           </div>
         </div>
-        <div className="card border-t-4  h-80 lg:h-72  border-[#2777C6] relative pt-8 bg-[#FFFFFF]  rounded-none card-compact  shadow-slate-400 shadow-xl">
+        <div className="card border-t-2  h-80 lg:h-72  border-[#2777C6] relative pt-8 bg-[#FFFFFF]  rounded-none card-compact shadow-xl">
           <div className="px-4">
-            <div className="">
+            <div className="my-2">
               <FaCalendarAlt className="w-16 text-[#2777C6] mx-auto h-16"></FaCalendarAlt>
             </div>
             <div className=" mt-2">
@@ -75,15 +77,15 @@ const SolutionsPage = () => {
               </p>
             </div>
           </div>
-          <div className="bg-[#F7F7F7] absolute w-full bottom-0   py-4">
+          <div className="bg-gray-100 absolute w-full bottom-0   py-4">
             <p className="px-4 ">
               Ideal for: Jobs with fixed payouts at a pre-defined interval
             </p>
           </div>
         </div>
-        <div className="card border-t-4  h-80 lg:h-72  border-[#2777C6] relative pt-8 bg-[#FFFFFF]  rounded-none card-compact  shadow-slate-400 shadow-xl">
+        <div className="card border-t-2 h-80 lg:h-72  border-[#2777C6] relative pt-8 bg-[#FFFFFF]  rounded-none card-compact shadow-xl">
           <div className="px-4">
-            <div className="">
+            <div className="my-2">
               <FaTasks className="w-16 text-[#2777C6] mx-auto h-16"></FaTasks>
             </div>
             <div className=" mt-2">
@@ -94,18 +96,18 @@ const SolutionsPage = () => {
               </p>
             </div>
           </div>
-          <div className="bg-[#F7F7F7] absolute w-full bottom-0   py-4">
+          <div className="bg-gray-100 absolute w-full bottom-0   py-4">
             <p className="px-4 ">
               Ideal for: Jobs with cost tracking at an individual task level
             </p>
           </div>
         </div>
       </div>
-      <div className="text-center mx-auto bg-[#F7F7F7] py-12">
+      <div className="text-center container mx-auto my-10 bg-[#F7F7F7] py-12">
         <h1 className=" text-3xl font-bold  my-4">Scope of Work</h1>
         <h1 className=" border-b-blue-600  w-12 border-2  my-4  mx-auto"></h1>
         <h1>Define the deliverables for the job, including:</h1>
-        <div className="text-left mx-auto grid grid-cols-2 lg:grid-cols-5 p-4 gap-8 mt-6">
+        <div className="text-left mx-auto grid grid-cols-2 lg:grid-cols-5 justify-center p-5 gap-8 mt-6">
           <h1 className="text-lg font-bold ">
             {" "}
             &#9989; Understanding of the job
